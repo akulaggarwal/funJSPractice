064e252b47f2b81bd800bd4b4529ef731eec0944
Akul Aggarwal <akul.aggarwal@gmail.com>
2016-11-05 18:43:02 -0700

    Initialize jasmine testing. Add .gitignore

--- mergeSort.js
+++ mergeSort.js
@@ -3,3 +3,7 @@
 const input = [1,5,7,3,7,8,3,8];
 console.log('input: ', input);
 console.log('input length: ', input.length);
+
+module.exports =  () => {
+  return 'hello';
+}
---
+++ spec/mergeSort_spec.js
@@ -0,0 +1,9 @@
+const merger = require('../mergeSort');
+
+describe("Hello World Server -", function() {
+
+  it("contains spec with an expectation", function() {
+    expect(merger()).toBe('hello');
+  });
+
+});
