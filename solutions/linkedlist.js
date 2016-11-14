828fe2f6f72eefa792d2c00187e45f6aaac9d1f5
Akul Aggarwal <akul.aggarwal@gmail.com>
2016-11-13 22:23:19 -0800

    linkedlist: finish

--- solutions/linkedlist.js
+++ solutions/linkedlist.js
@@ -1,8 +1,47 @@
 function generateLL(val0) {
   const val = {val: val0};
-  this.tail = val;
-  this.head = val;
+  this.tail = {next: val, val: null};
+  this.head = {val: null, next: val};
   val.next = this.tail;
 }
 
-const initialLL = new generateLL('5');
+generateLL.prototype.addVal = function(val) {
+  const newVal = {val, next: this.tail};
+  this.tail.next.next = newVal;
+  this.tail.next = newVal;
+}
+
+generateLL.prototype.find = function(val, remove) {
+  function deeper(obj, remove, oldObj) {
+    if (obj.val === null) {
+      return null;
+    }
+    if (val === obj.val) {
+      if (remove) {
+        return [obj, oldObj];
+      }
+      return obj;
+    }
+    return deeper(obj.next, remove, obj);
+  }
+  return deeper(this.head.next, remove, this.head);
+}
+
+generateLL.prototype.remove = function(val) {
+  const removeObj = this.find(val, true);
+  if (!removeObj) {
+    return "no such val";
+  }
+  const parent = removeObj[1];
+  const current = removeObj[0];
+  parent.next = current.next;
+  debugger;
+  current.next = null;
+}
+
+const newList = new generateLL(4);
+newList.addVal(6);
+newList.addVal(7);
+newList.addVal(8);
+const val7 = newList.find(7);
+newList.remove(7);
