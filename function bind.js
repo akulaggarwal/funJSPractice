d975bac17839d65b2bff6dc3f491d206632b6be0
Akul Aggarwal <akul.aggarwal@gmail.com>
2016-11-13 17:33:56 -0800

    funciton bind finished

---
+++ function bind.js
@@ -0,0 +1,47 @@
+var alice = {
+  name: 'alice',
+  shout: function (arg) {
+    console.log('here comes' + ' ' + this.name);
+  }
+};
+
+// alice.shout() //=> 'here comes alice'
+
+function bind1(func, context){
+	console.log('args: ', [...arguments]);
+	const args = [...arguments];
+	const after0 = args.slice(1,2);
+	// console.log([...arguments])
+	const after1 = args.slice(2);
+	console.log('aft0: ', after0);
+	console.log('aft1: ', after1);
+	return function(diff) {
+		const allArgs = [...after1, ...arguments];
+		console.log('after1 in anonymous func: ', allArgs);
+		return func.apply(context, allArgs);
+	}
+};
+
+// const boundShout = bind2(alice.shout, { name: 'bob' }, 'some arg') //=> 'here comes bob'
+
+
+// boundShout('mary', 'cat');
+////////////////////////////////////////
+
+
+Function.prototype.bind2 = function(context) {
+	const func = this;
+	const args = [...arguments];
+	const after0 = args.slice(1);
+	return function() {
+		const moreArgs = [...arguments];
+		const allArgs = [...after0, ...arguments];
+		return func.apply(context, allArgs)
+	}
+}
+
+var boundShout = alice.shout.bind(alice, 'arg0', 'arg1');
+boundShout('arg1', 'arg2'); // alerts 'alice'
+
+// boundShout = alice.shout.bind({ name: 'bob' });
+// boundShout(); // alerts 'bob'
