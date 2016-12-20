398752490e665fc72fb1607e4f7ea43f02902d3b
Akul Aggarwal <akul.aggarwal@gmail.com>
2016-12-20 15:26:10 -0800

    linearMotion: Finish render method

--- solutions/linearMotion.js
+++ solutions/linearMotion.js
@@ -2,10 +2,58 @@
 
 class Animation {
   animate(speed, init) {
-    this.r = 'a';
-    console.log(speed, init);
-    // console.log(this.r);
+    // let positisionsArr =;
+    const currentPositions = [...init].map( (v, i) => {
+      const obj = {L: 0, R: 0};
+      if (v === 'R') {
+        obj.R++
+      }
+      if (v === 'L') {
+        obj.L++
+      }
+      return obj;
+    })
+    console.log(currentPositions);
+
+    this.render(currentPositions);
+
+    /*while (currentPosition !== '.......') {
+      for (let i in currentPosition) {
+        const spot = currentPosition[i]; //will be R, L, or .
+
+        if (spot === 'L') {
+          this.moveLeft(i, speed);
+        }
+
+        if (spot === 'R') {
+          this.moveRight(i, speed);
+        }
+
+
+      }
+    }*/
   }
+
+  moveLeft() {
+
+  }
+
+  moveRight() {
+
+  }
+
+  render(currentPositions) {
+    const output = new Array(7);
+    output.fill('.')
+    currentPositions.forEach( (v,i) => {
+      if (v.L || v.R) {
+        output[i] = 'X';
+      }
+    })
+    console.log(output.join(''));
+  }
+
+
 }
 
 // Animation.prototype.animate = function(speed, init) {
