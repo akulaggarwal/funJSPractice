bb379d8b25c22a050235b0ab66292715ce4c45c6
Akul <akul@komprise.com>
2018-01-21 17:19:08 -0800

    Add mergeMeetignTimes solution.

--- solutions/iCake/mergingMeetingTimes_4.js
+++ solutions/iCake/mergingMeetingTimes_4.js
@@ -1,12 +1,26 @@
-function merge() {
+function merge(input) {
   //sort
-
+  input.sort((a, b) => {
+    return a.startTime - b.startTime;
+  });
 
   //merge
+  //push first slot
+  const merged = [input[0]];
+  //for ea. in input after first
+  for (let i = 1; i < input.length; i++) {
+    const lastMerged = merged[merged.length - 1];
+    const newSorted = input[i];
+    //push according to conditionals
+    if (lastMerged.endTime >= newSorted.startTime) {
+      lastMerged.endTime = Math.max(newSorted.endTime, lastMerged.endTime);
+    } else {
+      merged.push(newSorted);
+    }
 
-    //push first slot
-    //for ea. in input after first
-      //push according to conditionals
+  }
+
+  return merged;
 }
 
 const input = [
@@ -17,3 +31,5 @@ const input = [
   {startTime: 9,  endTime: 10},
 ];
 console.log(merge(input));
+
+//Comment: Provided solution is pretty much the same.
